3170482a7c76de47e7002acccb9870dfb7f719f9
Kalle <70443911+francescokalle@users.noreply.github.com>
2026-03-14 23:56:38 +0100

    Move Reports page content to Experiments page

---
+++ src/app/[lang]/knowledge/experiments/page.tsx
@@ -0,0 +1,13 @@
+// Returning experiments data
+import React from 'react';
+
+const ExperimentsPage = () => {
+    return (
+        <div>
+            <h1>Experiments</h1>
+            {/* Content of the original reports page goes here */}
+        </div>
+    );
+};
+
+export default ExperimentsPage;
